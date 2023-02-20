4c3038aff5067d3ef816c1a0a1e9c634db0c3e80
Rebecca VandeWater <becca.vandewater@gmail.com>
2023-02-20 15:42:46 -0700

    added js and func

---
+++ test/app.js
@@ -0,0 +1,32 @@
+
+function trySwitch(banana){
+
+    switch(banana) {
+        case "Jared":
+            console.log("Jared you suck at teaching!")
+            break;
+        case "Becca":
+            console.log("Hi Becca!")
+            break;
+        case "Dan":
+            console.log("Hi Dan!")
+            break;
+        default:
+            console.log("Who the fuck?")
+    }
+}
+
+function tryStatement(expression){
+    if(expression == "Dan"){
+        console.log("Ok then Dan..")
+    }
+    else if(expression == "Becca"){
+        console.log("Hey there.")
+    }
+    else if(expression == "Jared"){
+        console.log("Who invited you?")
+    }
+    else{
+        console.log("Get outta here scum")
+    }
+}
